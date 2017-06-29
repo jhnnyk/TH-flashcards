d5e8ca711e795e25481ce461f25fede556cc4483
John Knotts <jhnnyk@gmail.com>
2017-06-28 17:16:54 -0700

    dynamic content in pug templates

--- app.js
+++ app.js
@@ -8,8 +8,9 @@ app.get('/', (req, res) => {
   res.render('index')
 })
 
-app.get('/hello', (req, res) => {
-  res.send('<h1>Hello JavaScript Developer!</h1>')
+app.get('/cards', (req, res) => {
+  // res.locals.prompt = 'Who is buried in Grant\'s tomb?'
+  res.render('card', { prompt: 'Who is buried in Grant\'s tomb?' })
 })
 
 app.listen(3000, () => {
