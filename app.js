1d0024571eafdc768086cdca6be136157d4ae538
John Knotts <jhnnyk@gmail.com>
2017-06-29 21:13:20 -0700

    handling a POST request

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express()
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.set('view engine', 'pug')
 
@@ -11,6 +13,15 @@ app.get('/', (req, res) => {
 app.get('/cards', (req, res) => {
   // res.locals.prompt = 'Who is buried in Grant\'s tomb?'
   res.render('card', { prompt: 'Who is buried in Grant\'s tomb?' })
+})
+
+app.get('/hello', (req, res) => {
+  res.render('hello')
+})
+
+app.post('/hello', (req, res) => {
+  console.dir(req.body)
+  res.render('hello')
 })
 
 app.listen(3000, () => {
