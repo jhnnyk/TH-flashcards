ea919168754e64ed794bc6ac42d872eec6ff2318
John Knotts <jhnnyk@gmail.com>
2017-07-09 07:24:30 -0600

    fix redirection error

--- routes/cards.js
+++ routes/cards.js
@@ -20,7 +20,7 @@ router.get('/:id', (req, res) => {
   if (side === 'question') {
     templateData.hint = hint
   } else if (side != 'question' || side != 'answer') {
-    res.redirect(`/cards/${id}?side=question`)
+    return res.redirect(`/cards/${id}?side=question`)
   }
 
   res.render('card', templateData)
